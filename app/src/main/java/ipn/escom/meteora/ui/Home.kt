f8cd3fb7c780e0bdc5f5de871f498c11ac6d832f
Brandon Herrera <brandondh769@gmail.com>
2023-10-22 21:47:27 -0600

    Add navigation bar in home screen

--- app/src/main/java/ipn/escom/meteora/ui/Home.kt
+++ app/src/main/java/ipn/escom/meteora/ui/Home.kt
@@ -1,14 +1,39 @@
 package ipn.escom.meteora.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
-class Home {
-}
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import ipn.escom.meteora.R
 
 @Composable
 fun HomeScreen() {
-    Column {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf(R.string.menu_home, R.string.menu_map, R.string.menu_calendar)
+    val icons = listOf(R.drawable.home, R.drawable.map, R.drawable.calendar_month)
 
+    Column {
+        NavigationBar {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = icons[index]),
+                            contentDescription = stringResource(id = item)
+                        )
+                    },
+                    label = { Text(stringResource(id = item)) })
+            }
+        }
     }
 }
