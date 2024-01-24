9ac3cc3dc09a8331a3109a8e7354d46bc3cb35f1
Brandon Herrera <brandondh769@gmail.com>
2024-01-23 22:42:56 -0600

    Add forecast option in botton nav bar

--- app/src/main/java/ipn/escom/meteora/ui/Home.kt
+++ app/src/main/java/ipn/escom/meteora/ui/Home.kt
@@ -1,6 +1,11 @@
 package ipn.escom.meteora.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CalendarMonth
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.Map
+import androidx.compose.material.icons.rounded.WbSunny
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -11,7 +16,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import ipn.escom.meteora.R
@@ -19,8 +23,14 @@ import ipn.escom.meteora.R
 @Composable
 fun HomeScreen() {
     var selectedItem by remember { mutableIntStateOf(0) }
-    val items = listOf(R.string.menu_home, R.string.menu_map, R.string.menu_calendar)
-    val icons = listOf(R.drawable.home, R.drawable.map, R.drawable.calendar_month)
+    val items =
+        listOf(R.string.menu_home, R.string.menu_map, R.string.menu_agenda, R.string.menu_forecast)
+    val icons = listOf(
+        Icons.Rounded.Home,
+        Icons.Rounded.Map,
+        Icons.Rounded.CalendarMonth,
+        Icons.Rounded.WbSunny
+    )
 
     Column {
         Forecast(modifier = Modifier.weight(0.9f))
@@ -31,7 +41,7 @@ fun HomeScreen() {
                     onClick = { selectedItem = index },
                     icon = {
                         Icon(
-                            painter = painterResource(id = icons[index]),
+                            imageVector = icons[index],
                             contentDescription = stringResource(id = item)
                         )
                     },
