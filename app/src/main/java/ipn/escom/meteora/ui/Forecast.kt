a51a4773a88ed9e3799b497ca649304730ef240b
Brandon Herrera <brandondh769@gmail.com>
2024-03-20 11:48:41 -0600

    Add location request and locality display

--- app/src/main/java/ipn/escom/meteora/ui/Forecast.kt
+++ app/src/main/java/ipn/escom/meteora/ui/Forecast.kt
@@ -1,6 +1,10 @@
 package ipn.escom.meteora.ui
 
-import androidx.compose.foundation.background
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,15 +21,65 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import ipn.escom.meteora.R
 import ipn.escom.meteora.ui.theme.gray
+import kotlinx.coroutines.tasks.await
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.Locale
 
 @Composable
 fun Forecast(modifier: Modifier) {
+
+    val context = LocalContext.current
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+    var location by remember { mutableStateOf<Location?>(null) }
+    var municipality by remember { mutableStateOf<String?>(null) }
+    var postalCode by remember { mutableStateOf<String?>(null) }
+
+
+    LaunchedEffect(key1 = true) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            try {
+                val lastLocation = fusedLocationClient.lastLocation.await()
+                location = lastLocation
+                location?.let {
+                    val geocoder = Geocoder(context, Locale.getDefault())
+                    val addresses = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+                    if (addresses!!.isNotEmpty()) {
+                        municipality = addresses[0].locality
+                        postalCode = addresses[0].postalCode
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("LocationProvider", "Error getting location: ${e.message}", e)
+            }
+        } else {
+            Log.e("LocationProvider", "Location permissions not granted")
+        }
+    }
+
 
     Column(
         modifier = modifier.fillMaxSize()
@@ -40,9 +94,9 @@ fun Forecast(modifier: Modifier) {
                 containerColor = gray
             )
         ) {
-            Row (
+            Row(
                 modifier = Modifier.padding(8.dp)
-            ){
+            ) {
 
                 Icon(
                     imageVector = Icons.Outlined.LocationOn,
@@ -51,8 +105,10 @@ fun Forecast(modifier: Modifier) {
                     modifier = Modifier.size(20.dp)
                 )
                 Spacer(modifier = Modifier.width(8.dp))
+
                 Text(
-                    text = "Gustavo A. Madero",
+                    text = postalCode?.let { getLocalityFromPostalCode(it) }
+                        ?: "Ubicación no disponible",
                     style = MaterialTheme.typography.bodySmall,
                     modifier = Modifier.align(Alignment.CenterVertically),
                     color = Color.White
@@ -67,6 +123,21 @@ fun Forecast(modifier: Modifier) {
         )
         DailyWeather()
     }
+}
+
+@Composable
+fun getLocalityFromPostalCode(postalCode: String?): String? {
+    val context = LocalContext.current
+    val inputStream = context.resources.openRawResource(R.raw.postal_codes)
+    val reader = BufferedReader(InputStreamReader(inputStream))
+    var line: String?
+    while (reader.readLine().also { line = it } != null) {
+        val parts = line!!.split(",")
+        if (parts.size == 2 && parts[0] == postalCode) {
+            return parts[1]
+        }
+    }
+    return null
 }
 
 @Preview(showBackground = true, showSystemUi = true)
--- app/src/main/java/ipn/escom/meteora/ui/Home.kt
+++ app/src/main/java/ipn/escom/meteora/ui/Home.kt
@@ -1,5 +1,6 @@
 package ipn.escom.meteora.ui
 
+import android.content.pm.PackageManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.rounded.CalendarMonth
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Map
 import androidx.compose.material.icons.rounded.WbSunny
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,19 +29,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.google.firebase.auth.FirebaseAuth
+import ipn.escom.meteora.MainActivity
 import ipn.escom.meteora.R
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
@@ -56,6 +63,42 @@ fun Home(navController: NavController?) {
         Icons.Rounded.CalendarMonth,
         Icons.Rounded.WbSunny
     )
+
+    var isLocationPermissionGranted by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    if (ContextCompat.checkSelfPermission(
+            context,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    ) {
+        isLocationPermissionGranted = true
+    } else {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                context as MainActivity,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            AlertDialog(onDismissRequest = {
+                ActivityCompat.requestPermissions(
+                    context,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    1
+                )
+            }, title = { Text(text = "Permiso de ubicación") }, text = {
+                Text(text = "Se necesita el permiso de ubicación para mostrar el clima de tu ubicación actual")
+            }, confirmButton = { Text(text = "Aceptar") })
+        } else {
+            ActivityCompat.requestPermissions(
+                context,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
+        }
+
+    }
+
+
 
     Scaffold(
         topBar = {
