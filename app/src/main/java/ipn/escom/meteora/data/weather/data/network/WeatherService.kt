a1ef0d9ab398d62330bb8e03beabeb025c3cd75c
Brandon Herrera <brandondh769@gmail.com>
2024-04-17 12:17:11 -0600

    Fix Weather service timeout

--- app/src/main/java/ipn/escom/meteora/data/weather/data/network/WeatherService.kt
+++ app/src/main/java/ipn/escom/meteora/data/weather/data/network/WeatherService.kt
@@ -4,16 +4,26 @@ import android.util.Log
 import ipn.escom.meteora.core.network.RetrofitHelper
 import ipn.escom.meteora.data.weather.Weather
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import java.net.SocketTimeoutException
 
 class WeatherService {
     private val retrofit = RetrofitHelper.getRetrofit()
 
     suspend fun getWeather(apiKey: String, lat: Double, lon: Double): Weather? {
-        return withContext(Dispatchers.IO){
-            val response = retrofit.create(WeatherClient::class.java).getWeather(lat, lon, apiKey)
-            Log.d("WeatherService", "getWeather: ${response.body()}")
-            response.body()
+        return withContext(Dispatchers.IO) {
+            try {
+                val response =
+                    retrofit.create(WeatherClient::class.java).getWeather(lat, lon, apiKey)
+                Log.d("WeatherService", "getWeather: ${response.body()}")
+                response.body()
+            } catch (e: SocketTimeoutException) {
+                Log.e("WeatherService", "Timeout al conectar a la API")
+                delay(5000)
+                getWeather(apiKey, lat, lon)
+                null
+            }
         }
     }
 }
